16ffa1c1f3dc84af9d37cfd81f2e83fc70208003
Joe Sak <joe@joesak.com>
2013-12-31 18:52:29 -0500

    Learn about octokit auth, GET repo and Contributors graph data

---
+++ github-api/octokit_spec.rb
@@ -0,0 +1,26 @@
+require 'octokit'
+
+describe "Auth" do
+  it "should read from the .netrc" do
+    client = Octokit::Client.new netrc: true
+
+    client.login.should == "infoactive"
+  end
+end
+
+describe "GET a repo" do
+  it "returns the JSON response of the resource" do
+    repo = Octokit.repo 'mongodb/mongo'
+
+    repo.name.should == 'mongo'
+  end
+end
+
+describe "GET a repo Graph" do
+  it "should be found in the repo attrs" do
+    repo = Octokit.repo 'mongodb/mongo'
+    author = repo.rels[:contributors].get.data[0]
+
+    author.attrs[:login].should == 'erh'
+  end
+end
